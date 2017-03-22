f82f5fe75201de5b7a1bdb924d8643aac2325e8e
Spencer <spencer@Spencers-MacBook-Air.local>
2017-03-21 22:29:18 -0600

    it works

--- Pirate Boys/Assets/Scripts/HexGrid.cs
+++ Pirate Boys/Assets/Scripts/HexGrid.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class HexGrid : MonoBehaviour 
 {
 	public Transform hexPrefab;
-	public int ringDepth = 1;
+	public Material landMaterial;
+	public Material tier3Material;
+	public Material tier2Material;
+	public int islandDepth = 1;
+	public int tier3Depth = 2;
+	public int tier2Depth = 3;
 
 	public int gridWidth = 15;
 	public int gridHeight = 15;
@@ -14,10 +20,13 @@ public class HexGrid : MonoBehaviour
 	public float gap = 0.0f;
 
 	Vector3 startPos;
-	private List<Transform> hexes;
+
+	Transform[][] hexes;
+	GameObject centerHex;
 
 	void Start()
 	{
+		hexes = new Transform[gridHeight][];
 		AddGap();
 		CalcStartPos();
 		CreateGrid();
@@ -58,19 +67,104 @@ public class HexGrid : MonoBehaviour
 		Transform hex;
 		for (int y = 0; y < gridHeight; y++)
 		{
+			Transform[] row = new Transform[gridWidth];
 			for (int x = 0; x < gridWidth; x++)
 			{
-				Hexagon h = new Hexagon (x, y);
+				Vector2 h = new Vector2 (x, y);
 				hex = Instantiate (hexPrefab) as Transform;
-				hex.GetComponent<HexagonManager> ().setHexagon (h);
-				hexes.Add (hex);
+				hex.GetComponent<Hexagon> ().setHexagon (h);
 
 				Vector2 gridPos = new Vector2(x, y);
 				hex.position = CalcWorldPos(gridPos);
 				hex.parent = this.transform;
 				hex.name = "Hexagon" + x + "|" + y;
 
+				row [x] = hex;
 			}
+			hexes [y] = row;
+		}
+		populateIsland ();
+		populateTier3 ();
+		populateTier2 ();
+	}
+
+	GameObject getCenterHex() {
+		int centerX = (int) Mathf.Floor (gridWidth / 2);
+		int centerY = (int) Mathf.Floor (gridHeight / 2);
+		return hexes [centerX] [centerY].gameObject;
+	}
+
+	void populateIsland() {
+		centerHex = getCenterHex();
+		Debug.Log ("Center: " + centerHex);
+		centerHex.GetComponent<Renderer> ().material = landMaterial;
+
+		int n = 1;
+		while (n < islandDepth + 1) {
+			populateRing (landMaterial, n);
+			n += 1;
 		}
 	}
+
+	void populateTier3() {
+		int n = islandDepth + 1;
+		int top = n + tier3Depth;
+		while (n < top) {
+			Debug.Log (n);
+			populateRing (tier3Material, n);
+			n += 1;
+		}
+
+	}
+
+	void populateTier2() {
+		int n = islandDepth + tier3Depth + 1;
+		int top = n + tier2Depth;
+		while (n < top) {
+			populateRing (tier2Material, n);
+			n += 1;
+		}
+	}
+
+	void populateRing(Material mat, int ringNum) {
+
+		Vector2[] hexagonCoords = new Vector2 [ringNum * 6];
+
+		Hexagon currHexagon = getCenterHex ().GetComponent<Hexagon> ();
+		int n = 0;
+		while (n < ringNum) { // move to start hexagon
+			Vector2 next = currHexagon.getNext (5); // move west until we get to our ring start
+			currHexagon.setHexagon(next);
+			n += 1;
+		}
+
+		int index = 0;
+		for (int dir = 1; dir <= 6; ++dir) { // turn six times
+			for (int count = 0; count < ringNum; ++count) {
+				hexagonCoords [index] = currHexagon.getCoords();
+				Vector2 next = currHexagon.getNext (dir);
+				currHexagon.setHexagon (next);
+				index += 1;
+			}
+		}
+
+		// Reset hex coords to center
+		int centerX = (int) Mathf.Floor (gridWidth / 2);
+		int centerY = (int) Mathf.Floor (gridHeight / 2);
+		currHexagon.setHexagon(new Vector2(centerX, centerY));
+
+
+		foreach (var coord in hexagonCoords) {
+			int x = (int) coord.x;
+			int y = (int) coord.y;
+			try {
+				hexes [y] [x].gameObject.GetComponent<Renderer>().material = mat;
+			} catch (Exception e) {
+				// can't access that hex.
+			}
+			Debug.Log (coord);
+		}
+
+	}
+
 }
--- Pirate Boys/Assets/Scripts/Hexagon.cs
+++ Pirate Boys/Assets/Scripts/Hexagon.cs
@@ -2,41 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hexagon {
+public class Hexagon : MonoBehaviour {
 
-	private int x;
-	private int y;
+	public int x;
+	public int y;
 
 	public Hexagon(Vector2 coordinate) {
 		this.x = (int) coordinate.x;
 		this.y = (int) coordinate.y;
 	}
 
-	public Hexagon(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public void setHexagon(Vector2 vec) {
+		this.x = (int) vec.x;
+		this.y = (int) vec.y;
 	}
 
-	public Hexagon getNext(int direction) {
+	public Vector2 getNext(int direction) {
 		
 		int offsetX;
 		offsetX = ((this.y % 2) == 0) ? this.x : this.x+1;
 
+		// starting from W going NE
 		switch (direction) {
-		case 1: // NW
-			return new Hexagon(new Vector2(offsetX, this.y+1));
-		case 2: // NE
-			return new Hexagon(new Vector2(offsetX-1, this.y+1));
-		case 3: // E
-			return new Hexagon(new Vector2(this.x-1, this.y));
-		case 4: // SE
-			return new Hexagon(new Vector2(offsetX-1, this.y-1));
-		case 5: // SW
-			return new Hexagon(new Vector2(offsetX, this.y-1));
-		case 6: // W
-			return new Hexagon(new Vector2(this.x+1, this.y));
+		case 1: // NE
+			return new Vector2(offsetX-1, this.y+1);
+		case 2: // E
+			return new Vector2(this.x-1, this.y);
+		case 3: // SE
+			return new Vector2(offsetX-1, this.y-1);
+		case 4: // SW
+			return new Vector2(offsetX, this.y-1);
+		case 5: // W
+			return new Vector2(this.x+1, this.y);
+		case 6: // NW
+			return new Vector2(offsetX, this.y+1);
 		default:
-			return null;
+			return new Vector2 (0, 0);
 		}
+	}
+
+	public Vector2 getCoords() {
+		return new Vector2 (x, y);
 	}
 }
